307da01b55ec8c6c4583946756a161a2fa18aacb
Shikhar Sharma <ceoshikhar@gmail.com>
2021-04-04 19:26:20 +0530

    feat: gatbsy to replace react project and use gatsby cloud instead firebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,49 @@
 module.exports = {
   siteMetadata: {
-    title: "Test",
+    site: `CEOShikhar`,
+    title: `Shikhar Sharma's small space on the internet`,
+    titleTemplate: `%s - CEOShikhar`,
+    description: `My name is Shikhar Sharma AKA CEOShikhar. I am an entrepreneur, developer and an artist. Welcome to my small space on the internet.`,
+    siteUrl: `https://www.ceoshikhar.com`,
+    language: `en`,
+    color: `#0A0A0A`,
+    twitter: 'ceoshikhar',
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
-};
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-routes`,
+      options: {
+        path: `${__dirname}/src/routes.ts`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Shikhar Sharma's small space on the internet`,
+        short_name: `CEOShikhar`,
+        start_url: `/`,
+        background_color: `#0A0A0A`,
+        theme_color: `#047DC4`,
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        include_favicon: true, // Include favicon
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+  ],
+  flags: {
+    DEV_SSR: false,
+  },
+}
---
+++ src/containers/index.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
+
+import Layout from '../layout/index'
+
+import Heading from '../components/Heading'
+
+interface IndexPageProps {
+  location: {
+    pathname: string
+  }
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 24px;
+`
+
+const Email = styled.a`
+  margin-left: 0.35em;
+  text-decoration: none;
+  color: ${(props) => props.theme.color.primary};
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+export default ({ location }: IndexPageProps) => {
+  return (
+    <Layout location={location}>
+      <Wrapper>
+        <Heading title="🚧 I AM WORKING HERE! I AM WORKING HERE!" />
+        <p>
+          Untill I am done working here, you can hit me up on
+          <Email
+            href="mailto: ceoshikhar@gmail.com"
+            style={{ marginLeft: '0.35em' }}
+          >
+            ceoshikhar@gmail.com
+          </Email>
+        </p>
+      </Wrapper>
+    </Layout>
+  )
+}
+
+export const indexPageQuery = graphql`
+  query IndexPageQuery {
+    image: file(relativePath: { eq: "icon.png" }) {
+      ...fluidImage
+    }
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
