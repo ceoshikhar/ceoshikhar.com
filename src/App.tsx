1043feff3630890f01d982a431c298ac1154b604
Shikhar Sharma <ceoshikhar@gmail.com>
2021-04-04 12:37:33 +0530

    refactor: not show avatar, instead use construction emoji

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 function App() {
   return (
     <main>
-      <img
-        src="/favicon.jpg"
-        alt="my avatar"
-        height="200px"
-        style={{ borderRadius: '50%' }}
-      />
-      <h1>I AM WORKING HERE! I AM WORKING HERE!</h1>
+      <h1>🚧 I AM WORKING HERE! I AM WORKING HERE!</h1>
       <p>
         Untill I am done working here, you can hit me up on
         <a href="mailto: ceoshikhar@gmail.com" style={{ marginLeft: '0.35em' }}>
