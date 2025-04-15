33f429a555255b36188b1cfd5b1fd2e8be202789
Shikhar Sharma <ceoshikhar@gmail.com>
2025-04-15 17:59:55 +0530

    fix: styled-components imports

--- src/styles/styleUtils.ts
+++ src/styles/styleUtils.ts
@@ -1,24 +1,25 @@
-import { css, ThemedCssFunction, DefaultTheme } from "styled-components";
+import { css, StyleFunction, DefaultTheme } from "styled-components";
 
 export const sizes = {
-  xl: 1366,
-  lg: 1024,
-  md: 768,
-  sm: 480
+    xl: 1366,
+    lg: 1024,
+    md: 768,
+    sm: 480,
 };
 
 export const media = (Object.keys(sizes) as Array<keyof typeof sizes>).reduce(
-  (accumulator, label) => {
-    const emSize = sizes[label] / 16;
-    accumulator[label] = (first: any, ...interpolations: any[]) => css`
-      @media (max-width: ${emSize}em) {
-        ${css(first, ...interpolations)}
-      }
-    `;
-    return accumulator;
-  },
-  // eslint-disable-next-line no-unused-vars
-  {} as { [key in keyof typeof sizes]: ThemedCssFunction<DefaultTheme> }
+    (accumulator, label) => {
+        const emSize = sizes[label] / 16;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        accumulator[label] = (first: any, ...interpolations: any[]) => css`
+            @media (max-width: ${emSize}em) {
+                ${css(first, ...interpolations)}
+            }
+        `;
+        return accumulator;
+    },
+
+    {} as { [key in keyof typeof sizes]: StyleFunction<DefaultTheme> }
 );
 
 export const px2em = (px: number) => `${px / 16}em`;
